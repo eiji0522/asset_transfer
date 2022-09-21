16191a41e9a985bbd5c8ea9b9a1edb017248025d
Eiji <eiji0522@gmail.com>
2022-09-22 00:58:23 +0900

    Merging Frondend and Backend

--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {products} from '../../products';
+import { ProductService } from '../shared/product.service';
 
 @Component({
   selector: 'app-product-listings',
@@ -10,9 +10,18 @@ export class ProductListComponent implements OnInit {
 
   products: any
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
-    this.products = products
+
+    const productObservable = this.productService.getProducts()
+    productObservable.subscribe(
+      (data) => {
+        this.products = data
+        console.log('次のデータが出力されました ' + data)
+      },
+      (err) => {'次のエラーが発生しました:' + err}
+    )
+
     }
 }
