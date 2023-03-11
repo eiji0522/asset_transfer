3d11299fc26985b344f50433f75d74d1837d0cf5
Eiji <eiji0522@gmail.com>
2023-03-11 22:15:47 +0900

    Login-module adding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { HttpClientModule } from '@angular/common/http';
   declarations: [
     AppComponent,
     NavbarComponent,
-    ProductComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
