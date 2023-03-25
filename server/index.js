8d31d776008033da45b04a9d65abcaa6224f7bef
Eiji <eiji0522@gmail.com>
2023-03-25 23:34:19 +0900

    Heroku Set-up

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
@@ -28,16 +29,17 @@ app.use(bodyParser.json())
 app.use('/api/v1/products', productRoutes)
 app.use('/api/v1/users', userRoutes)
 
-if(process.env.NODE_ENV === 'production') {
-    const appPath = path.join( __dirname, '..', 'dist', 'asset_transfer')
-    app.use(express.static(appPath))
-    app.get("*", function(req, res){
-        res.sendFile(path.resolve(appPath, 'index.html'))
-    })
-}
+  if(process.env.NODE_ENV === 'production') {
+     const appPath = path.join( __dirname, '..', 'dist', 'asset_transfer')   
+     app.use(express.static(appPath))   
+     app.get("*", function(req, res){
+         res.sendFile(path.resolve(appPath, 'index.html'))
+     })
+  }
 
 const PORT = process.env.PORT || '3001'
 
 app.listen( PORT, function(){
     console.log('I am running!')
+    console.log(app.get('env'))
 })
